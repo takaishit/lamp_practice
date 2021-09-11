b9637d67bcafd3f9312e9de0d50525a7dd7a4f03
Takuya Takaishi <crown_imperial@icloud.com>
2021-09-11 21:22:57 +0900

    SQL文修正しました

--- lamp_dock/append_schema.sql
+++ lamp_dock/append_schema.sql
@@ -4,10 +4,11 @@
 CREATE TABLE `orders` (
     `id` int(11) AUTO_INCREMENT,
     `user_id` int(11) NOT NULL,
-    `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---購入詳細画面
+-- 購入詳細画面
 
 CREATE TABLE `order_details`(
     `order_id` int(11) NOT NULL,
